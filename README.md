8858ea1486dc06102a0a23cc07c1766e64296b72
Jon LaBelle <jon@tech0.com>
2015-01-25 10:57:34 -0600

    Init commit.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Sublime HTML Encoder
+
+A [Sublime Text](http://www.sublimetext.com) plug-in to encode and decode [HTML character entities](http://dev.w3.org/html5/html-author/charref).
+
+## Examples
+
+### *Encode* HTML Character Entities
+
+- **Named**: `&` to `&amp;`
+- **Hexadecimal**: `&` to `&#x00026;`
+- **Decimal**: `&` to `&#38;`
+
+### *Decode* HTML Character Entities
+
+- **Named**: `&amp;` to `&`
+- **Hexadecimal**: `&#x00026;` to `&`
+- **Decimal**: `&#38;` to `&`
+
+## Install
+
+Download the [zip](https://github.com/jonlabelle/SublimeHtmlDecode/archive/master.zip)
+to your Sublime Text **Packages** directory.
+
+## Usage
+
+HTML Decode and Encode commands can be accessed from the Sublime
+Text command palette:
+
+- `ctrl/cmd + shift + P` : ***HTML Encode***
+- `ctrl/cmd + shift + P` : ***HTML Decode***
+
+## Author
+
+Jon LaBelle <contact@jonlabelle.com>
+
+## License
+
+Sublime HTML Encoder is licensed under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ html_encoder.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import sublime
+import sublime_plugin
+
+PY_MAJOR_VER = sys.version_info[0]
+PY_MINOR_VER = sys.version_info[1]
+
+
+class HtmlDecodeCommand(sublime_plugin.TextCommand):
+
+    def decode_html(self, str):
+        # Use unescape from py standard lib
+        # see: http://stackoverflow.com/questions/2087370
+        if PY_MAJOR_VER == 2:
+            # python 2
+            from HTMLParser import HTMLParser
+            return HTMLParser().unescape(str)
+        elif PY_MAJOR_VER == 3 and PY_MINOR_VER <= 3:
+            # python 3
+            from html.parser import HTMLParser
+            return HTMLParser().unescape(str)
+        else:
+            # python 3.4+
+            from html import unescape
+            return unescape(str)
+
+    def selections(self, view, default_to_all=True):
+        regions = [r for r in view.sel() if not r.empty()]
+        if not regions and default_to_all:
+            regions = [sublime.Region(0, view.size())]
+        return regions
+
+    def run(self, edit):
+        view = self.view
+        source_text = decoded_text = ''
+
+        for region in self.selections(view):
+            source_text = decoded_text = view.substr(region)
+            if len(source_text) > 0:
+                decoded_text = self.decode_html(source_text)
+
+        if source_text == decoded_text:
+            sublime.set_timeout(lambda: sublime.status_message('Nothing to decode.'), 0)
+        else:
+            view.replace(edit, region, decoded_text)
+            sublime.set_timeout(lambda: sublime.status_message('HTML Decoded.'), 0)
+
+
+class HtmlEncodeCommand(sublime_plugin.TextCommand):
+
+    def encode_html(self, str):
+        if PY_MAJOR_VER >= 3:
+            from html import escape
+            return escape(str)
+        else:
+            # use saxutils escape for python 2
+            from xml.sax.saxutils import escape
+            html_escape_table = {'"': "&quot;", "'": "&apos;"}
+            str = escape(str, html_escape_table)
+            str = str.decode('utf-8')
+            str = str.encode('ascii', 'xmlcharrefreplace')
+            return str
+
+    def selections(self, view, default_to_all=True):
+        regions = [r for r in view.sel() if not r.empty()]
+        if not regions and default_to_all:
+            regions = [sublime.Region(0, view.size())]
+        return regions
+
+    def run(self, edit):
+        view = self.view
+        source_text = encoded_text = ''
+
+        for region in self.selections(view):
+            source_text = encoded_text = view.substr(region)
+            if len(source_text) > 0:
+                encoded_text = self.encode_html(source_text)
+
+        if source_text == encoded_text:
+            sublime.set_timeout(lambda: sublime.status_message('Nothing to encode.'), 0)
+        else:
+            view.replace(edit, region, encoded_text)
+            sublime.set_timeout(lambda: sublime.status_message('HTML Encoded.'), 0)
