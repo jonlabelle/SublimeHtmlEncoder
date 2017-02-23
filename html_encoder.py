f8e6955a4c6adf916cc0ba34f83cf803d8698655
Jon LaBelle <contact@jonlabelle.com>
2017-02-23 03:48:15 -0600

    Try to prevent double-encoding.

--- html_encoder.py
+++ html_encoder.py
@@ -15,69 +15,76 @@ def selections(view, default_to_all=True):
     return regions
 
 
+def encode_html(txt):
+    if PY_MAJOR_VER >= 3:
+        from html import escape
+        return escape(txt)
+    else:
+        # use saxutils escape for python 2
+        from xml.sax.saxutils import escape
+        html_escape_table = {'"': "&quot;", "'": "&apos;"}
+        txt = escape(txt, html_escape_table)
+        txt = txt.decode('utf-8')
+        txt = txt.encode('ascii', 'xmlcharrefreplace')
+        return txt
+
+
+def decode_html(txt):
+    # Use unescape from py standard lib
+    # see: http://stackoverflow.com/questions/2087370
+    if PY_MAJOR_VER == 2:
+        # python 2
+        from HTMLParser import HTMLParser
+        return HTMLParser().unescape(txt)
+    elif PY_MAJOR_VER == 3 and PY_MINOR_VER <= 3:
+        # python 3
+        from html.parser import HTMLParser
+        return HTMLParser().unescape(txt)
+    else:
+        # python 3.4+
+        from html import unescape
+        return unescape(txt)
+
+
 class HtmlDecodeCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
+        file_changed = False
 
         for region in selections(view):
-            source_text = decoded_text = view.substr(region)
-            if len(source_text) > 0:
-                decoded_text = self.decode_html(source_text)
+            source = view.substr(region)
+            transformed = decode_html(source)
+            if source != transformed:
+                view.replace(edit, region, transformed)
+                file_changed = True
 
-            if source_text == decoded_text:
-                sublime.set_timeout(lambda: sublime.status_message(
-                    'HTML Encoder: Nothing to decode.'), 0)
-            else:
-                view.replace(edit, region, decoded_text)
-                sublime.set_timeout(lambda: sublime.status_message(
-                    'HTML Encoder: HTML Decoded.'), 0)
-
-    @staticmethod
-    def decode_html(txt):
-        # Use unescape from py standard lib
-        # see: http://stackoverflow.com/questions/2087370
-        if PY_MAJOR_VER == 2:
-            # python 2
-            from HTMLParser import HTMLParser
-            return HTMLParser().unescape(txt)
-        elif PY_MAJOR_VER == 3 and PY_MINOR_VER <= 3:
-            # python 3
-            from html.parser import HTMLParser
-            return HTMLParser().unescape(txt)
+        if file_changed is True:
+            sublime.set_timeout(lambda: sublime.status_message(
+                'HTML Encoder: HTML Decoded.'), 0)
         else:
-            # python 3.4+
-            from html import unescape
-            return unescape(txt)
+            sublime.set_timeout(lambda: sublime.status_message(
+                'HTML Encoder: Nothing to decode.'), 0)
 
 
 class HtmlEncodeCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
+        file_changed = False
 
         for region in selections(view):
-            source_text = encoded_text = view.substr(region)
-            if len(source_text) > 0:
-                encoded_text = self.encode_html(source_text)
+            source = view.substr(region)
 
-            if source_text == encoded_text:
-                sublime.set_timeout(lambda: sublime.status_message(
-                    'HTML Encoder: Nothing to encode.'), 0)
-            else:
-                view.replace(edit, region, encoded_text)
-                sublime.set_timeout(
-                    lambda: sublime.status_message(
-                        'HTML Encoder: HTML Encoded.'), 0)
+            # decode first to prevent double-encoding:
+            source_decoded = decode_html(source)
 
-    @staticmethod
-    def encode_html(txt):
-        if PY_MAJOR_VER >= 3:
-            from html import escape
-            return escape(txt)
+            transformed = encode_html(source_decoded)
+            if source != transformed:
+                view.replace(edit, region, transformed)
+                file_changed = True
+
+        if file_changed is True:
+            sublime.set_timeout(lambda: sublime.status_message(
+                'HTML Encoder: HTML Encoded.'), 0)
         else:
-            # use saxutils escape for python 2
-            from xml.sax.saxutils import escape
-            html_escape_table = {'"': "&quot;", "'": "&apos;"}
-            txt = escape(txt, html_escape_table)
-            txt = txt.decode('utf-8')
-            txt = txt.encode('ascii', 'xmlcharrefreplace')
-            return txt
+            sublime.set_timeout(lambda: sublime.status_message(
+                'HTML Encoder: Nothing to encode.'), 0)
